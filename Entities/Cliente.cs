490c5df4acc650eab282c52df9c16c252552811b
wilsonfelix <wilson.felix.jr@gmail.com>
2020-12-02 21:38:51 -0300

    Projeto01 commit

---
+++ Entities/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_01.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+}
---
+++ Repositories/ClienteRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Projeto_01.Entities;
+using System.IO;
+
+namespace Projeto_01.Repositories
+{
+    public class ClienteRepository
+    {
+        private string path = @"h:\Projetos\Repositorio_local\COTI\Projeto_01\Projeto_01\Cliente.txt";
+        
+        public void GravarDados(Cliente cliente)
+        {
+            using (var Stream_Writer = new StreamWriter(path, true))
+            {
+                Stream_Writer.WriteLine("Id...........: " + cliente.Id);
+                Stream_Writer.WriteLine("Nome.........: " + cliente.Nome);
+                Stream_Writer.WriteLine("Email........: " + cliente.Email);
+                Stream_Writer.WriteLine("CPF..........: " + cliente.Cpf);
+                Stream_Writer.WriteLine("...");
+
+
+            }
+        }
+
+        public string LerDados()
+        {
+            using (var Stream_Reader = new StreamReader(path))
+            {
+                return Stream_Reader.ReadToEnd();
+            }
+        }
+    }
+}
